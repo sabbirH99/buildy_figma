b31da093a810dd60b2ea78ca10066607fbc69208
sabbirH99 <132889029+sabbirH99@users.noreply.github.com>
2023-05-12 04:57:12 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -38,3 +38,55 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+// video playing in testimonial section
+
+playBtn = document.querySelector(".btn-play");
+video = document.querySelector("video");
+
+playBtn.addEventListener("click", function(){
+    video.play();
+    playBtn.style.display = "none";
+});
+
+video.addEventListener("click", () => {
+    video.pause();
+    playBtn.style.display = "block";
+});
+
+
+// testmonial slider
+
+let slider = document.querySelector(".slider");
+let slides = slider.querySelectorAll(".slide");
+let currentSlide = 0;
+let slideInterval = setInterval(showNextSlide, 4000); // storing the interval for later use like reseting the interval
+let prevBtn = document.querySelector(".btn.prev");
+let nextBtn = document.querySelector(".btn.next");
+
+prevBtn.addEventListener("click", showPrevSlide);
+nextBtn.addEventListener("click", showNextSlide);
+
+
+// function for previous slide
+function showPrevSlide() {
+    resetInterval();
+    slides[currentSlide].classList.remove("active");
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    slides[currentSlide].classList.add("active");
+}
+
+// function for next slide and default slider
+
+
+function showNextSlide() {
+    resetInterval();
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
+
+function resetInterval() {
+    clearInterval(slideInterval); // clear the previous interval
+    slideInterval = setInterval(showNextSlide, 5000); // start a new interval
+}
